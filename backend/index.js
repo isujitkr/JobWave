3c6b97451e4125605e16e6ae8522568c5371be24
isujitkr <skr87981@gmail.com>
2024-08-07 01:46:07 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import {connectDB} from './utils/db.js';
 
 const app = express();
 const PORT = 5000;
@@ -10,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors());
+
+//db connection
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Api Working');
